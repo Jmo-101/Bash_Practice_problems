0677690894397fab2b58e942f304ad0cee2bea32
Jorge Molina <138607757+Jmo-101@users.noreply.github.com>
2023-08-30 16:55:25 -0400

    Create Date_script.sh

---
+++ Date_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Make a cronjob to schedule the script to run every friday at 11 pm. Programmed the crontab as 0 23 * * 5
+
+crontab -e
+
+#update the server with these commands
+
+sudo apt install && sudo apt update 
+
+#list out the amount of packages that can be upgraded
+
+package_upgrades=$(apt list --upgradable)
+
+#Write a command to iterate the line on how many packages are upgradable and create a new file with the information
+
+echo "$package_upgrades can be upgraded" > update8.30.23.txt
+
+#show the contents of the new file
+
+cat update8.30.23.txt
