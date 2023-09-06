b868b12e355bb9e7d3d5a90a6e1011bf4881496b
Jorge Molina <138607757+Jmo-101@users.noreply.github.com>
2023-09-06 17:43:15 -0400

    Update ssh_scp.sh

--- ssh_scp.sh
+++ ssh_scp.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#making a function to ssh into a server using the variable ssh_server i used in the menu above, gave variables to both the read commands in order to use it i>
+
+ssh_server(){
+read "enter the ssh user: " ssh_username
+read "enter the server ip address: " ssh_IP
+ssh $ssh_username@$ssh_IP
+}
+
+
+#made a function for scp and made options for the user to choose from
+scp_copy(){
+read -p "enter the ssh user:" scp_username
+read -p "enter the ssh ip address: " scp_IP
+
+read -p "choose file destination (1: remote to local, 2: local to remote): " file_destination
+}
+
+
 
 #making a menu for the user to choose from, gave a variable of 'option' to the read(input) command in order to use it below in a case statement
 echo "select an option"
@@ -21,10 +39,10 @@ case $option in
 
 esac
 
-#making a function to ssh into a server using the variable ssh_server i used in the menu above, gave variables to both the read commands in order to use it i>
+# #making a function to ssh into a server using the variable ssh_server i used in the menu above, gave variables to both the read commands in order to use it i>
 
-ssh_server(){
-read "enter the ssh user: " ssh_username
-read "enter the server ip address: " ssh_IP
-ssh $ssh_username@$ssh_IP
-}
+# ssh_server(){
+# read "enter the ssh user: " ssh_username
+# read "enter the server ip address: " ssh_IP
+# ssh $ssh_username@$ssh_IP
+# }
